ac6a2effbd6e2229e3f64711255511e214bc2936
wladengine <wladengine@lenovo>
2023-04-17 08:31:24 +0300

    Market fix and visual improvements

--- src/pages/market/Market.js
+++ src/pages/market/Market.js
@@ -1,15 +1,34 @@
 import React, { useEffect } from 'react';
+import {
+    Grid, Box, Stack, Card, CardHeader, CardContent, Typography, CircularProgress, Backdrop, Slider
+} from "@mui/material";
 import AutocompleteCombobox from "../../components/autocomplete-combobox/AutocompleteCombobox";
-import Box from '@mui/material/Box';
-import Stack from "@mui/material/Stack";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import Typography from "@mui/material/Typography";
-import CardContent from "@mui/material/CardContent";
+
+const FoundClaimRecord = ({productName, realmName, feeName, clause}) => {
+    return (
+        <Card>
+            <CardHeader
+                subheader={productName}
+            />
+            <CardContent>
+                <Typography variant="body1" color="text.secondary">
+                    {realmName}
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                    Минимальная сумма: {feeName}
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                    {clause}
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
 
 const Market = () => {
     const [realms, setRealms] = React.useState(null)
     const [actors, setActors] = React.useState(null)
+    const [noFilterSelected, setNoFilterSelected] = React.useState(true)
     const [foundClaims, setFoundClaims] = React.useState(null)
     const [realmsParams, setRealmsParams] = React.useState(-1)
     const [actorsParams, setActorsParams] = React.useState(-1)
@@ -60,21 +79,28 @@ const Market = () => {
         let realm_id = -1;
         let actor_id = -1;
 
-        if (typeof realmsParams !== "undefined" || realmsParams == null) {
+        if (typeof realmsParams === "undefined" || realmsParams == null) {
             realm_id = -1;
         }
-        else if (typeof realmsParams.id !== "undefined" || realmsParams.id == null) {
+        else if (typeof realmsParams.id === "undefined" || realmsParams.id == null) {
             realm_id = -1;
         }
-
-        if (typeof actorsParams !== "undefined" || actorsParams == null) {
-            actor_id = -1;
-        }
-        else if (typeof actorsParams.id !== "undefined" || actorsParams.id == null) {
-            actor_id = -1;
+        else {
+            realm_id = realmsParams.id
         }
 
+        if (typeof actorsParams === "undefined" || actorsParams == null) {
+            actor_id = -1;
+        }
+        else if (typeof actorsParams.id === "undefined" || actorsParams.id == null) {
+            actor_id = -1;
+        }
+        else {
+            actor_id = actorsParams.id
+        }
+        console.log(`realm_id=${realm_id}; actor_id=${actor_id} sumParams=${sumParams}`)
         if (realm_id !== -1 || actor_id !== -1 || sumParams != 10) {
+            setNoFilterSelected(false)
             let urlRealmsFilters = ''
             if (realm_id !== -1) {
                 urlRealmsFilters = `&realm_id=${realm_id}`
@@ -89,6 +115,7 @@ const Market = () => {
             }
             let fetchUrl = `${baseUrl}/claims?${urlRealmsFilters}${urlActorsFilters}${urlSumParams}`
             console.log(fetchUrl)
+            backdropOpen()
             fetch(fetchUrl, {
                 method: 'GET', // *GET, POST, PUT, DELETE, etc.
                 mode: 'cors',
@@ -107,8 +134,10 @@ const Market = () => {
                 .catch(function (error) {
                     console.log(error)
                 })
+                .finally(() => { backdropClose() })
         } else {
             setFoundClaims(null)
+            setNoFilterSelected(true)
         }
     }, [realmsParams, actorsParams, sumParams])
 
@@ -126,7 +155,7 @@ const Market = () => {
         '2 млрд',
         '3 млрд',
         '5 млрд',
-        '5 млрд',
+        '10 млрд',
     ]
 
     const realmsOptions =
@@ -164,38 +193,45 @@ const Market = () => {
                 })
 
     const foundClaimsList =
-        foundClaims && foundClaims.map((val, index) => {
-                return (
-                    <Card key={index}>
-                        <CardHeader
-                            subheader={`[${val.product.short_name}] ${val.product.name}`}
-                        />
-                        <CardContent>
-                            <Typography variant="body2" color="text.secondary">
-                                {realmsParams == -1 ? val.realm.description : ''}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Минимальная сумма: {val.fee.name}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                {val.clause}
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                )
-            })
+        foundClaims && foundClaims.map((val, index) => <FoundClaimRecord
+            key={index}
+            productName={`[${val.product.short_name}] ${val.product.name}`}
+            realmName={realmsParams?.id !== -1 ? val.realm.description : ''}
+            feeName={val.fee.name}
+            clause={val.clause}
+        />)
     const foundClaimsCount =
-        foundClaims == null ? 'не найдено' : `найдено вариантов: ${foundClaims.length}`
+        foundClaims == null ? ( noFilterSelected ? 'не выбрано фильтров' : 'не найдено') : `найдено вариантов: ${foundClaims.length}`
 
     const sumParamsView =
         typeof sumParams == 'undefined' || typeof sumsValues[sumParams] == 'undefined'
             ? 'любой'
             : `от ${sumsValuesText[sumParams]} рублей`
 
+    const getSumParamsView = (value) =>
+        typeof value == 'undefined' || typeof sumsValues[value] == 'undefined'
+        ? 'любой'
+        : `от ${sumsValuesText[value]} рублей`
+    const handleChange = (event, newValue) => {
+        if (typeof newValue === 'number') {
+            setSumParams(newValue);
+        }
+    }
+
+    const [backdropVisible, setBackdropVisible] = React.useState(false);
+    const backdropClose = () => { setBackdropVisible(false) }
+    const backdropOpen = () => { setBackdropVisible(true) }
+
     return (
-        <>
-            <div className={'CRow'}>
-                <div className={'CCol'} xs={11}>
+        <Grid container>
+            <Grid item xs={12}>
+                <Backdrop
+                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                    open={backdropVisible}
+                >
+                    <CircularProgress color="inherit" />
+                </Backdrop>
+                <Stack spacing={1}>
                     <Box mb={2}>
                         <AutocompleteCombobox
                             id={'realm'}
@@ -216,35 +252,35 @@ const Market = () => {
                     </Box>
                     <Box mb={2}>
                         <Box>
-                            <b htmlFor="customRange2" className="form-label">
+                            <Typography variant="h6" color="text.secondary">
                                 Укажите объём инвестирования
-                            </b>
+                            </Typography>
                         </Box>
                         <Box>
-                            <input
-                                type="range"
-                                className="form-range"
-                                min="0"
-                                max="10"
-                                id="customRange2"
-                                onChange={(e) => {
-                                    setSumParams(e.target.value)
-                                }}
+                            <Slider
+                                value={sumParams}
+                                min={0}
+                                step={1}
+                                max={10}
+                                getAriaValueText={getSumParamsView}
+                                valueLabelFormat={getSumParamsView}
+                                onChange={handleChange}
+                                valueLabelDisplay="auto"
                             />
                         </Box>
                         <Box>
-                            <span>{sumParamsView}</span>
+                            <Typography variant="body1" color="text.secondary">{sumParamsView}</Typography>
                         </Box>
                     </Box>
-                </div>
-            </div>
-            <div className={'CRow'}>
-                <Stack spacing={1}>
-                    <b>{foundClaimsCount}</b>
+                </Stack>
+            </Grid>
+            <Grid item xs={12}>
+                <Stack spacing={2}>
+                    <Typography variant="h6" color="text.primary">{foundClaimsCount}</Typography>
                     {foundClaimsList}
                 </Stack>
-            </div>
-        </>
+            </Grid>
+        </Grid>
     )
 }
 
